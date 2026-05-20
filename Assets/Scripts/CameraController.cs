a548da2ec16ade519493769cdb75afc84a3f70d1
caleb.serranoobando@education.nsw.gov.au <caleb.serranoobando@education.nsw.gov.au>
2026-05-20 12:45:51 +1000

    goooooonk

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,25 @@
-using UnityEngine;
+// using UnityEngine;
+// 
+// public class CameraController : MonoBehaviour
+// {
+//     public GameObject player;
+//     private Vector3 offset;
+//     public Transform target; // The object to follow
+//     public float xRotation = 0f;
+//     int DistanceAway = 10;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         Vector3 PlayerPOS = GameObject.Find("Player").transform.transform.position;
+//     }
+// 
+//     // Update is called once per frame
+//     void LateUpdate() {
+//         // Follow position
+//         transform.position = target.position + offset;
+//         transform.rotation = Quaternion.Euler(xRotation, 0f, 0f);
+//     }
+// }
 
-public class CameraController : MonoBehaviour
-{
-    public GameObject player;
-    private Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
-}
+
